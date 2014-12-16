1c377bbb7161d49ebfcedf4e91632e2baaa3c3fd
Jeff Hemminger <jeff@atware.co.jp>
2014-12-16 11:07:38 +0900

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "Shiny Spice"
+
+version := "0.0.1"
+
+scalaVersion := "2.11.1"
---
+++ src/main/scala/shiny/spice/Shiny.scala
@@ -0,0 +1,9 @@
+package shiny.spice
+
+object Shiney {
+
+  def main(args: Array[String]) {
+    hellow
+  }
+  def hellow = println("Hello World!")
+}
