dbb1f71aba84bc71c8bb5c4d1d65e50c4d52d4ce
Jeff Hemminger <jeff@atware.co.jp>
2015-01-04 08:36:23 +0900

    code cleanup, further testing

--- src/main/scala/shiny/spice/WeatherRetriever.scala
+++ src/main/scala/shiny/spice/WeatherRetriever.scala
@@ -9,25 +9,27 @@ class WeatherRetriever() {
   val url = "http://api.openweathermap.org/data/2.5/history/city"
 
   val cities = List(
-    ("1848354","Asia/Tokyo"),
-    ("4887158","America/Chicago"),
-    ("4887398","America/Chicago"),
-    ("5028509","America/Chicago"),
-    ("4128894","America/Chicago"))
+    ("1848354","Asia/Tokyo")) //,
+//    ("4887158","America/Chicago"),
+//    ("4887398","America/Chicago"),
+//    ("5028509","America/Chicago"),
+//    ("4128894","America/Chicago"))
 
   def retrieveWeather(cities: List[(String,String)]) {
     val city = cities.head
 
     for(i <- 167 to 1 by -1) {
-      val time = unixTime(city._2, i)
 
-      val params = List(
-        ("id", city._1),
-        ("type", "hour"),
-        ("start", time.toString),
-        ("cnt", "1"))
-
-      val weather = Http(url).timeout(connTimeoutMs = 5000, readTimeoutMs = 15000).params(params).asString
+      val weather =
+        Http(url).timeout(
+          connTimeoutMs = 5000,
+          readTimeoutMs = 15000)
+          .params(
+          List(
+            ("id", city._1),
+            ("type", "hour"),
+            ("start", unixTime(city._2, i)),
+            ("cnt", "1"))).asString
 
       val json:JsValue = Json.parse(weather.body)
 
@@ -39,10 +41,7 @@ class WeatherRetriever() {
     }
   }
 
-  def unixTime(zone:String, minus:Int) : Int = {
-    val ut:Int =
-      (DateTime.now(DateTimeZone.forID(zone)).minusHours(minus).getMillis() / 1000).toInt
-    println(ut)
-    return ut
+  def unixTime(zone:String, minus:Int) : String = {
+    return ((DateTime.now(DateTimeZone.forID(zone)).minusHours(minus).getMillis() / 1000).toInt).toString
   }
 }
